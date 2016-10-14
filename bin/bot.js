1ce8a5bd7b31767875d9975564a7c1a717df9e53
Alexei Avrutin <alexavrutin@gmail.com>
2016-10-14 02:55:32 +0200

    Changed init channel

--- bin/bot.js
+++ bin/bot.js
@@ -6,11 +6,13 @@ var Pmbot = require('../lib/pmbot');
 var token = process.env.BOT_API_KEY;
 var dbPath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
+var initChannel = process.env.INIT_CHANNEL;
 
 var Pmbot = new Pmbot({
   token: token,
   dbPath: dbPath,
-  name: name
+  name: name,
+  initChannel: initChannel
 });
 
 Pmbot.run();
